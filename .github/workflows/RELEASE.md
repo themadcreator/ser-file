ce775b972318e93aa0422f47a1bf294a1a255765
themadcreator <themadcreator@github>
2026-03-11 14:43:32 -0700

    workflow syntax

--- .github/workflows/RELEASE.md
+++ .github/workflows/RELEASE.md
@@ -4,9 +4,6 @@ cargo set-version $VER
 
 # Push current version tag
 ```bash
-    VER="0.1.0-pre3"
-    cargo set-version $VER
-    git add Cargo.toml Cargo.lock
-    git commit -m "Bump version ${VER}"
+    VER="0.1.0-pre10"
     ./.github/workflows/release.sh
 ```
--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-if [ -n "$(git status --porcelain)" ]; then
-    echo "Git working directory is dirty. Aborting tag creation."
-else
-    VER=$(cargo metadata --no-deps --format-version=1 | jq -r '.packages[0].version')
-    echo "Releasing version ${VER}"
-    echo "Press [ENTER] when ready..."
-    read  -n 1 
-    git tag -a "v${VER}" -m "Release version ${VER}"
-    git push origin --tags
+
+if [[ -n "$VER" ]]; then
+    echo 'The environment variable VER is not set. Aborting.'
+    exit 1
 fi
+
+if [ -n "$(git status --porcelain)" ]; then
+    echo "Git working directory is dirty. Aborting."
+    exit 1
+fi
+
+cargo set-version $VER
+git add Cargo.toml Cargo.lock
+git commit -m "Bump version ${VER}"
+
+# VER=$(cargo metadata --no-deps --format-version=1 | jq -r '.packages[0].version')
+
+echo "Releasing version ${VER}"
+echo "Press [ENTER] when ready..."
+read  -n 1 
+
+git tag -a "v${VER}" -m "Release version ${VER}"
+git push origin --tags
