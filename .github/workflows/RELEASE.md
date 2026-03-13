f6d5bc9bdacdf08bf10f50999f37f311668d05ee
themadcreator <themadcreator@github>
2026-03-13 12:39:28 -0700

    release script

--- .github/workflows/RELEASE.md
+++
@@ -1,4 +0,0 @@
-# Push current version tag
-```bash
-    export VER="0.1.1-pre.1"; ./.github/workflows/release.sh
-```
--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-if [[ -z "$VER" ]]; then
-    echo 'The environment variable VER is not set. Aborting.'
-    exit 1
-fi
-
 if [ -n "$(git status --porcelain)" ]; then
     echo "Git working directory is dirty. Aborting."
     exit 1
 fi
 
-cargo set-version $VER
+ver=$(cargo metadata --no-deps --format-version=1 | jq -r '.packages[0].version')
+
+echo "\n\nCurrent version is $ver\n\n"
+
+read -p "What to bump? (patch/rc) " bump
+cargo set-version --dry-run --bump $bump
+
+while true; do
+    read -p "Proceed with this change and release? (y/n) " yn
+    case $yn in
+        [Yy]* ) echo "Confirmed."; break;; # Handles 'y', 'Y', 'yes', 'Yes', etc.
+        * ) exit;;
+    esac
+done
+
+cargo set-version --bump $bump
+ver=$(cargo metadata --no-deps --format-version=1 | jq -r '.packages[0].version')
+
+echo "Releasing version ${ver}"
+
 git add Cargo.toml Cargo.lock
-git commit -m "Bump version ${VER}"
-
-echo "Releasing version ${VER}"
-echo "Press [ENTER] when ready..."
-read -n 1
-
+git commit -m "Bump version ${ver}"
 git push
-git tag -a "v${VER}" -m "Release version ${VER}"
+
+git tag -a "v${ver}" -m "Release version ${ver}"
 git push origin --tags
